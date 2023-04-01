cb5dd808bc13d472a5778beb7a5b608838b5f873
Mario488 <78291860+Mario488@users.noreply.github.com>
2023-04-01 12:51:35 +0300

    delete

--- hotel_home/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HotelHomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hotel_home'
--- reservations/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from . models import Reservations
-# Register your models here.
-admin.site.register(Reservations)
